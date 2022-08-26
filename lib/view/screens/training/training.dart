82856971bae7b7ea3d41ce7945a5ba03db07242e
Eddie98 <eddie.morra1234@gmail.com>
2022-08-26 17:02:55 +0600

    little fix willpop on training

--- lib/view/screens/training/training.dart
+++ lib/view/screens/training/training.dart
@@ -58,91 +58,98 @@ class _TrainingScreenState extends State<TrainingScreen> {
     }
   }
 
+  Future<bool> _backBtnHandle(BuildContext context) {
+    trainingBloc.add(const TrainingInitialEvent());
+    Navigator.pop(context);
+    return Future.value(false);
+  }
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        leading: ArrowBackBtnWidget(
-          () => Navigator.pop(context),
+    return WillPopScope(
+      onWillPop: () => _backBtnHandle(context),
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          leading: ArrowBackBtnWidget(() => _backBtnHandle(context)),
+          actions: [
+            const SettingsButton(isFromTrainingScreen: true),
+            sizedBoxWidth(defaultHorPadding),
+          ],
+          title: Text(trainingAppbarText(settingsRepo.nBackValue)),
         ),
-        actions: [
-          const SettingsButton(isFromTrainingScreen: true),
-          sizedBoxWidth(defaultHorPadding),
-        ],
-        title: Text(trainingAppbarText(settingsRepo.nBackValue)),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: BlocBuilder<TrainingBloc, TrainingState>(
-          builder: (context, state) {
-            if (state is TrainingProccess) {
-              return Row(
-                children: [
-                  FooterButton(
-                    index: 0,
-                    text: positionText,
-                    onTap: state.isPositionBtnDisabled
-                        ? null
-                        : () => trainingBloc
-                            .add(const TrainingPositionBtnClickEvent()),
-                  ),
-                  FooterButton(
-                    index: 1,
-                    text: colorText,
-                    onTap: state.isColorBtnDisabled
-                        ? null
-                        : () => trainingBloc
-                            .add(const TrainingColorBtnClickEvent()),
-                  ),
-                ],
+        bottomNavigationBar: BottomAppBar(
+          child: BlocBuilder<TrainingBloc, TrainingState>(
+            builder: (context, state) {
+              if (state is TrainingProccess) {
+                return Row(
+                  children: [
+                    FooterButton(
+                      index: 0,
+                      text: positionText,
+                      onTap: state.isPositionBtnDisabled
+                          ? null
+                          : () => trainingBloc
+                              .add(const TrainingPositionBtnClickEvent()),
+                    ),
+                    FooterButton(
+                      index: 1,
+                      text: colorText,
+                      onTap: state.isColorBtnDisabled
+                          ? null
+                          : () => trainingBloc
+                              .add(const TrainingColorBtnClickEvent()),
+                    ),
+                  ],
+                );
+              }
+              return const SizedBox.shrink();
+            },
+          ),
+        ),
+        body: SafeArea(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return BlocConsumer<TrainingBloc, TrainingState>(
+                listenWhen: (oldState, newState) {
+                  if (newState is TrainingProccess) {
+                    return newState.counter > settingsRepo.totalAttempts;
+                  }
+                  return true;
+                },
+                listener: (context, state) {
+                  if (state is TrainingProccess) {
+                    Navigator.of(context).pushReplacementNamed(
+                      Routes.resultsLink,
+                      arguments: [state.correctAnswers, state.wrongAnswers],
+                    );
+                    trainingBloc.add(const TrainingInitialEvent());
+                  }
+                },
+                buildWhen: (oldState, newState) {
+                  if (newState is TrainingProccess) {
+                    return newState.counter <= settingsRepo.totalAttempts;
+                  }
+                  return true;
+                },
+                builder: (context, state) {
+                  if (state is TrainingProccess) {
+                    return Column(
+                      children: [
+                        sizedBoxHeight(18.0),
+                        CountersRowWidget(state),
+                        sizedBoxHeight(22.0),
+                        BoardWidget(state, constraints.maxHeight),
+                      ],
+                    );
+                  }
+                  return const SizedBox.shrink();
+                },
               );
-            }
-            return const SizedBox.shrink();
-          },
-        ),
-      ),
-      body: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return BlocConsumer<TrainingBloc, TrainingState>(
-              listenWhen: (oldState, newState) {
-                if (newState is TrainingProccess) {
-                  return newState.counter > settingsRepo.totalAttempts;
-                }
-                return true;
-              },
-              listener: (context, state) {
-                if (state is TrainingProccess) {
-                  Navigator.of(context).pushReplacementNamed(
-                    Routes.resultsLink,
-                    arguments: [state.correctAnswers, state.wrongAnswers],
-                  );
-                  trainingBloc.add(const TrainingInitialEvent());
-                }
-              },
-              buildWhen: (oldState, newState) {
-                if (newState is TrainingProccess) {
-                  return newState.counter <= settingsRepo.totalAttempts;
-                }
-                return true;
-              },
-              builder: (context, state) {
-                if (state is TrainingProccess) {
-                  return Column(
-                    children: [
-                      sizedBoxHeight(18.0),
-                      CountersRowWidget(state),
-                      sizedBoxHeight(22.0),
-                      BoardWidget(state, constraints.maxHeight),
-                    ],
-                  );
-                }
-                return const SizedBox.shrink();
-              },
-            );
-          },
+            },
+          ),
         ),
       ),
     );
