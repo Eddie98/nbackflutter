db42b01d8973c013d213c2b38308d90401d5dfcd
Eddie98 <eddie.morra1234@gmail.com>
2023-01-28 11:46:25 +0600

    deploy

--- lib/view/screens/introdution/introdution.dart
+++ lib/view/screens/introdution/introdution.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:nbackflutter/constants/index.dart';
 import 'package:nbackflutter/routes.dart';
@@ -15,86 +14,79 @@ class IntrodutionScreen extends StatelessWidget {
 
     final mediaQuery = MediaQuery.of(context);
 
-    return SafeArea(
-      top: false,
-      minimum: defaultTargetPlatform == TargetPlatform.iOS
-          ? EdgeInsets.only(
-              bottom: getPropScreenWidth(24.0),
-            )
-          : EdgeInsets.zero,
-      child: Scaffold(
-        backgroundColor: AppColors.mainBlackColor,
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              pinned: true,
-              snap: false,
-              floating: false,
-              expandedHeight: mediaQuery.size.height * .24,
-              automaticallyImplyLeading: false,
-              elevation: 0,
-              actions: [
-                const SettingsButton(isFromTrainingScreen: false),
-                sizedBoxWidth(defaultHorPadding),
-              ],
-              flexibleSpace: LayoutBuilder(builder: (context, constraints) {
-                final isCollapsed = constraints.biggest.height <=
-                    mediaQuery.padding.top + kToolbarHeight + defaultHorPadding;
+    return Scaffold(
+      backgroundColor: AppColors.mainBlackColor,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            snap: false,
+            floating: false,
+            expandedHeight: mediaQuery.size.height * .24,
+            automaticallyImplyLeading: false,
+            elevation: 0,
+            actions: [
+              const SettingsButton(isFromTrainingScreen: false),
+              sizedBoxWidth(defaultHorPadding),
+            ],
+            flexibleSpace: LayoutBuilder(builder: (context, constraints) {
+              final isCollapsed = constraints.biggest.height <=
+                  mediaQuery.padding.top + kToolbarHeight + defaultHorPadding;
 
-                return FlexibleSpaceBar(
-                  title: Container(
-                    height: kToolbarHeight,
-                    alignment: isCollapsed
-                        ? Alignment.centerLeft
-                        : Alignment.bottomLeft,
-                    child: const TextRenderer(
-                      style: TextRendererStyle.header1,
-                      text: mainTitle,
-                      child: Text(mainTitle),
-                    ),
+              return FlexibleSpaceBar(
+                title: Container(
+                  height: kToolbarHeight,
+                  alignment:
+                      isCollapsed ? Alignment.centerLeft : Alignment.bottomLeft,
+                  child: const TextRenderer(
+                    style: TextRendererStyle.header1,
+                    text: mainTitle,
+                    child: Text(mainTitle),
                   ),
-                  titlePadding: EdgeInsetsDirectional.only(
-                    bottom: isCollapsed
-                        ? 0.0
-                        : getPropScreenWidth(defaultHorPadding),
-                    start: getPropScreenWidth(defaultHorPadding),
-                  ),
-                  centerTitle: false,
-                  background: const DecoratedBox(
-                    decoration: BoxDecoration(
-                      color: AppColors.mainBlackColor,
-                      boxShadow: [
-                        BoxShadow(
-                          color: AppColors.mainBlackColor,
-                          blurRadius: 0.0,
-                          spreadRadius: 1.0,
-                          offset: Offset(0, 0),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              }),
-            ),
-            SliverToBoxAdapter(
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: getPropScreenWidth(defaultHorPadding),
-                  vertical: getPropScreenWidth(defaultHorPadding) * 2,
                 ),
-                child: TextRenderer(
-                  style: TextRendererStyle.paragraph,
-                  text: mainDescription,
-                  child: Text(
-                    mainDescription,
-                    style: TextStyles.mainDescriptionTS(),
+                titlePadding: EdgeInsetsDirectional.only(
+                  bottom:
+                      isCollapsed ? 0.0 : getPropScreenWidth(defaultHorPadding),
+                  start: getPropScreenWidth(defaultHorPadding),
+                ),
+                centerTitle: false,
+                background: const DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: AppColors.mainBlackColor,
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppColors.mainBlackColor,
+                        blurRadius: 0.0,
+                        spreadRadius: 1.0,
+                        offset: Offset(0, 0),
+                      ),
+                    ],
                   ),
+                ),
+              );
+            }),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: getPropScreenWidth(defaultHorPadding),
+                vertical: getPropScreenWidth(defaultHorPadding) * 2,
+              ),
+              child: TextRenderer(
+                style: TextRendererStyle.paragraph,
+                text: mainDescription,
+                child: Text(
+                  mainDescription,
+                  style: TextStyles.mainDescriptionTS(),
                 ),
               ),
             ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
+          ),
+        ],
+      ),
+      floatingActionButton: Align(
+        alignment: const Alignment(1, .9),
+        child: FloatingActionButton(
           onPressed: () {
             Navigator.of(context).pushNamed(Routes.trainingLink);
           },
